b5a84f29cfda014a954442b0f35e78a0deaa558e
jcduff00 <johncduffman@gmail.com>
2023-07-28 17:18:15 -0400

    rewrote logic to reflect updated test url

--- parallelscraper/main.go
+++ parallelscraper/main.go
@@ -4,63 +4,90 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 type PollData struct {
-	PollSource string `json:"poll_source"`
-	Approve    string `json:"approve"`
-	Disapprove string `json:"disapprove"`
+	Source          string `json:"source"`
+	DatesAdminister string `json:"dates_administered"`
+	DatesUpdated    string `json:"dates_updated"`
+	JoeBiden        string `json:"joe_biden"`
+	RFKJr           string `json:"robert_f_kennedy_jr"`
+	Marianne        string `json:"marianne_williamson"`
+	OtherUndecided  string `json:"other_undecided"`
+	Margin          string `json:"margin"`
 }
 
 func main() {
-	var spreadsheetURL string
+	var url string
 
-	flag.StringVar(&spreadsheetURL, "url", "", "URL of the spreadsheet to scrape")
+	flag.StringVar(&url, "url", "", "URL of the webpage to scrape")
 	flag.Parse()
 
-	if spreadsheetURL == "" {
-		fmt.Println("Error: Please provide the URL of the spreadsheet using the -url flag.")
+	if url == "" {
+		fmt.Println("Error: Please provide the URL of the webpage using the -url flag.")
 		return
 	}
 
 	c := colly.NewCollector()
 	var data []PollData
 
-	c.OnHTML("tr", func(e *colly.HTMLElement) {
-		cells := e.DOM.Children()
-		if cells.Length() == 3 {
-			pollSource := strings.TrimSpace(cells.Eq(0).Text())
-			approve := strings.TrimSpace(cells.Eq(1).Text())
-			disapprove := strings.TrimSpace(cells.Eq(2).Text())
-
-			pollData := PollData{
-				PollSource: pollSource,
-				Approve:    approve,
-				Disapprove: disapprove,
-			}
-
-			data = append(data, pollData)
+	// Extract the poll data from the table
+	c.OnHTML(".wikitable tbody tr", func(e *colly.HTMLElement) {
+		if e.Index == 0 {
+			return // Skip the header row
 		}
+
+		source := strings.TrimSpace(e.ChildText("td:nth-child(1)"))
+		datesAdminister := strings.TrimSpace(e.ChildText("td:nth-child(2)"))
+		datesUpdated := strings.TrimSpace(e.ChildText("td:nth-child(3)"))
+		joeBiden := strings.TrimSpace(e.ChildText("td:nth-child(4)"))
+		rfkJr := strings.TrimSpace(e.ChildText("td:nth-child(5)"))
+		marianne := strings.TrimSpace(e.ChildText("td:nth-child(6)"))
+		otherUndecided := strings.TrimSpace(e.ChildText("td:nth-child(7)"))
+		margin := strings.TrimSpace(e.ChildText("td:nth-child(8)"))
+
+		pollData := PollData{
+			Source:          source,
+			DatesAdminister: datesAdminister,
+			DatesUpdated:    datesUpdated,
+			JoeBiden:        joeBiden,
+			RFKJr:           rfkJr,
+			Marianne:        marianne,
+			OtherUndecided:  otherUndecided,
+			Margin:          margin,
+		}
+
+		data = append(data, pollData)
 	})
 
-	if err := c.Visit(spreadsheetURL); err != nil {
+	c.OnScraped(func(r *colly.Response) {
+		// Convert data to JSON format
+		jsonData, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			log.Fatal("Error converting data to JSON:", err)
+		}
+
+		// Write JSON data to a file
+		file, err := os.Create("poll_data.json")
+		if err != nil {
+			log.Fatal("Error creating JSON file:", err)
+		}
+		defer file.Close()
+
+		_, err = file.Write(jsonData)
+		if err != nil {
+			log.Fatal("Error writing JSON data to file:", err)
+		}
+
+		fmt.Println("Data scraped successfully and saved in poll_data.json!")
+	})
+
+	if err := c.Visit(url); err != nil {
 		log.Fatal(err)
 	}
-
-	jsonData, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		log.Fatal("Error converting data to JSON:", err)
-	}
-
-	err = ioutil.WriteFile("poll_data.json", jsonData, 0644)
-	if err != nil {
-		log.Fatal("Error writing JSON data to file:", err)
-	}
-
-	fmt.Println("Data scraped successfully and saved in poll_data.json!")
 }
